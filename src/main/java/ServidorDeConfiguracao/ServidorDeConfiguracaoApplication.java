141eaeb6ca3e7cf3beb2773dcff60e5fc08ec445
Mateus <bittenmat@gmail.com>
2022-06-23 15:52:23 -0300

    Projeto iniciado

---
+++ src/main/java/ServidorDeConfiguracao/ServidorDeConfiguracaoApplication.java
@@ -0,0 +1,13 @@
+package ServidorDeConfiguracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorDeConfiguracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorDeConfiguracaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ServidorDeConfiguracao/ServidorDeConfiguracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package ServidorDeConfiguracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorDeConfiguracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
